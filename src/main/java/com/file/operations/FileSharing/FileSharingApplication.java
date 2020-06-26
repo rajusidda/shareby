e4f88637b2df56c760832da25eb747bb4e62913a
Sidda Raju <sraju@HYD-LAP-00322.local>
2020-06-27 00:00:53 +0530

    first commit

---
+++ src/main/java/com/file/operations/FileSharing/FileSharingApplication.java
@@ -0,0 +1,13 @@
+package com.file.operations.FileSharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileSharingApplication.class, args);
+	}
+
+}
