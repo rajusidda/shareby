d6198ee7ecc571b38c33ec443e8ae2c925cdef62
Sidda Raju <sraju@HYD-LAP-00322.local>
2020-06-27 01:49:23 +0530

    updated the test class and package names

--- src/test/java/com/file/shareby/SharedbyApplicationTests.java
+++ src/test/java/com/file/shareby/SharedbyApplicationTests.java
@@ -1,15 +1,13 @@
-/*
-package com.file.operations.FileSharing;
+package com.file.shareby;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FileSharingApplicationTests {
+class SharedbyApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
 }
-*/
